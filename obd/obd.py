a8c868d9cc7e483ad511f8bb88d15b9c3a833f5b
sommersoft <sommersoft@users.noreply.github.com>
2016-09-04 22:15:37 -0400

    --

--- obd/obd.py
+++ obd/obd.py
@@ -311,7 +311,8 @@ class OBD(object):
     def __build_command_string(self, cmd):
         """ assembles the appropriate command string """
 
-        logger.info("BCS cmd: %s >>> %s" % cmd.command, (str(self.__frame_counts[cmd]).encode()))
+        logger.info("BCS cmd: %s" % cmd.command)
+        logger.info("Encoded _frame_counts: %s" % str(self.__frame_counts[cmd]).encode())
         cmd_string = cmd.command
 
         # if we know the number of frames that this command returns,
