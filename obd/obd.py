ed9c721dbd886861f69b8143896e0c8370151776
sommersoft <sommersoft@users.noreply.github.com>
2016-09-07 21:53:12 -0400

    --

--- obd/obd.py
+++ obd/obd.py
@@ -359,7 +359,7 @@ class OBD(object):
                 return OBDResponse()
 
             for i in range(len(messages)):
-                logger.info("Message rcvd: %s" % OBDCommand(messages[i]))
+                logger.info("Message rcvd: %s" % OBDResponse(messages[i]))
             logger.info("cmd_msg{}: %s" % cmd_msg)
     
             #return cmd(messages) # compute a response object
