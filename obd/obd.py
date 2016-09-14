01c654b50029f805484099d9fbb1068851da9a71
sommersoft <sommersoft@users.noreply.github.com>
2016-09-14 00:21:03 -0400

    still working TypeError at line 392

--- obd/obd.py
+++ obd/obd.py
@@ -360,8 +360,6 @@ class OBD(object):
             # message.
             # @brendan-w wrote this newer version
             master = messages[0] # the message that contains our response
-            for frame in master.frames:
-                print unhexlify(frame)
             mode = master.data.pop(0) # the mode byte (ie, for mode 01 this would be 0x41)
             
             cmds_by_pid = { cmd.pid:cmd for cmd in cmds }
