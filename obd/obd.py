37b480af8da0c3649e4bc991ac50964190c02460
sommersoft <sommersoft@users.noreply.github.com>
2016-09-14 00:17:08 -0400

    --

--- obd/obd.py
+++ obd/obd.py
@@ -360,7 +360,7 @@ class OBD(object):
             # message.
             # @brendan-w wrote this newer version
             master = messages[0] # the message that contains our response
-            print hexlify(master.frames)
+            print master.frames
             mode = master.data.pop(0) # the mode byte (ie, for mode 01 this would be 0x41)
             
             cmds_by_pid = { cmd.pid:cmd for cmd in cmds }
